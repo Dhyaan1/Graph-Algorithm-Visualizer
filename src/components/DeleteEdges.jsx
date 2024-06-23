1e67d4ec94cda5611b0ec5437dc2bca9c0e8f3c2
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-06-23 21:11:34 +0530

    chore: Add DeleteEdges component for managing graph edges

---
+++ src/components/DeleteEdges.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Button1 from "./Buttons/Button1";
+
+export default function DeleteEdges({
+  myEdges,
+  setMyEdges,
+  selections,
+  setSelections,
+}) {
+  const [disableDeleteEdge, setDisableDeleteEdge] = useState(true);
+
+  useEffect(() => {
+    if (selections.length === 0) {
+      setDisableDeleteEdge(true);
+    }
+    if (selections.length !== 1) {
+      setDisableDeleteEdge(true);
+    }
+    if (selections.length === 1 && selections[0].includes("->")) {
+      setDisableDeleteEdge(false);
+    }
+  }, [selections]);
+  function DeleteEdge(EdgeId) {
+    const newEdges = myEdges.filter((edge) => edge.id !== EdgeId);
+    setMyEdges(newEdges);
+    setDisableDeleteEdge(true);
+    setSelections([]);
+  }
+
+  return (
+    <>
+      <Button1
+        disabled={disableDeleteEdge}
+        onClick={() => DeleteEdge(selections[0])}
+      >
+        Delete Edge
+      </Button1>
+    </>
+  );
+}
