7ec654964f672b36c4d30967aa52a19491f5dbd5
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-06-24 21:01:06 +0530

    feat: Add DeleteNodes component for managing graph nodes

---
+++ src/components/DeleteNodes.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { DeleteNode } from "../functions/NodeFunctions";
+import Button1 from "./Buttons/Button1";
+import { useState, useEffect } from "react";
+
+export default function DeleteNodes({
+  selections,
+  myNodes,
+  myEdges,
+  setMyEdges,
+  setMyNode,
+  setSelections,
+}) {
+  const [disableDeleteNode, setDisableDeleteNode] = useState(true);
+  useEffect(() => {
+    if (selections.length === 0) {
+      setDisableDeleteNode(true);
+    }
+    if (selections.length > 1) {
+      setDisableDeleteNode(true);
+    }
+    //only for single node selection
+    if (selections.length === 1 && !selections[0].includes("->")) {
+      setDisableDeleteNode(false);
+    }
+    //only for two node selection
+  }, [selections]);
+  return (
+    <>
+      <Button1
+        disabled={disableDeleteNode}
+        onClick={() =>
+          DeleteNode(
+            selections[0],
+            myNodes,
+            myEdges,
+            setMyEdges,
+            setMyNode,
+            setDisableDeleteNode,
+            setSelections
+          )
+        }
+      >
+        Delete Node
+      </Button1>
+    </>
+  );
+}
