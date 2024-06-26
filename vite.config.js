b5de9c914aa46e1221704eb12582543a29f394b6
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-06-26 14:57:36 +0530

    Set up Shadcn UI along with Shadcn tabs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/graph-algorithm-visualizer",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
