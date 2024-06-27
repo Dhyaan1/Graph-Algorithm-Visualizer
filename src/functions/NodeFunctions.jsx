bf8fb2bab375ac0c1bcb4558d5f044cd6baec59e
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-06-27 14:43:59 +0530

    feat: Refactor createAdjacencyGraph function to ensure a new array is created and set, so that UseEffect is triggered when AdjacencyMatrix changes

--- src/functions/NodeFunctions.jsx
+++ src/functions/NodeFunctions.jsx
@@ -28,56 +28,72 @@ function createAdjacencyGraph(
       graph[target][source] = label;
     }
   });
-  setAdjacencyMatrix(graph);
+  // Ensure a new array is created and set so that UseEffect is triggered
+  setAdjacencyMatrix([...graph]);
   console.log("Adjacency Matrix", graph);
 }
 
-  function DeleteNode(NodeId, myNodes, myEdges, setMyEdges, setMyNode, setDisableDeleteNode, setSelections) {
-    const newNodes = myNodes.filter((node) => node.id !== NodeId);
-    const newEdges = myEdges.filter(
-      (edge) => edge.source !== NodeId && edge.target !== NodeId
-    );
-    setMyEdges(newEdges);
-    setMyNode(newNodes);
-    setDisableDeleteNode(true);
-    setSelections([]);
-  }
+function DeleteNode(
+  NodeId,
+  myNodes,
+  myEdges,
+  setMyEdges,
+  setMyNode,
+  setDisableDeleteNode,
+  setSelections
+) {
+  const newNodes = myNodes.filter((node) => node.id !== NodeId);
+  const newEdges = myEdges.filter(
+    (edge) => edge.source !== NodeId && edge.target !== NodeId
+  );
+  setMyEdges(newEdges);
+  setMyNode(newNodes);
+  setDisableDeleteNode(true);
+  setSelections([]);
+}
 
-  function BFS(graph, start, visited, nodeCount) {
-    const queue = [];
-    const traversal = [];
-    queue.push(start);
-    visited[start] = true;
-    while (queue.length > 0) {
-      const node = queue.shift();
-      traversal.push(node);
-      for (let i = 1; i <= nodeCount; i++) {
-        if (graph[node][i] && !visited[i]) {
-          queue.push(i);
-          visited[i] = true;
-        }
+function BFS(graph, start, visited, nodeCount) {
+  const queue = [];
+  const traversal = [];
+  queue.push(start);
+  visited[start] = true;
+  while (queue.length > 0) {
+    const node = queue.shift();
+    traversal.push(node);
+    for (let i = 1; i <= nodeCount; i++) {
+      if (graph[node][i] && !visited[i]) {
+        queue.push(i);
+        visited[i] = true;
       }
     }
-    return traversal;
   }
-  function bfsForDisconnectedComponents(graph, nodeCount, myNodes) {
-    const visited = new Array(nodeCount + 1).fill(false);
-    const allTraversals = [];
+  return traversal;
+}
+function bfsForDisconnectedComponents(graph, nodeCount, myNodes) {
+  const visited = new Array(nodeCount + 1).fill(false);
+  const allTraversals = [];
 
-    myNodes.forEach((node) => {
-      if (!visited[node.id]) {
-        const temp = BFS(graph, node.id, visited);
-        allTraversals.push(temp);
-      }
-    });
-    return allTraversals;
-  }
-  function bfsCall(adjacencyMatrix, nodeCount, myNodes) {
-    const arr = bfsForDisconnectedComponents(adjacencyMatrix, nodeCount, myNodes);
-    console.log("BFS");
-    arr.forEach((traversal) => {
-      console.log("BFS", traversal);
-    });
-  }
+  myNodes.forEach((node) => {
+    if (!visited[node.id]) {
+      const temp = BFS(graph, node.id, visited);
+      allTraversals.push(temp);
+    }
+  });
+  return allTraversals;
+}
+function bfsCall(adjacencyMatrix, nodeCount, myNodes) {
+  const arr = bfsForDisconnectedComponents(adjacencyMatrix, nodeCount, myNodes);
+  console.log("BFS");
+  arr.forEach((traversal) => {
+    console.log("BFS", traversal);
+  });
+}
 
-export { CreateDFS, CreateBFS, create2DArray, createAdjacencyGraph, DeleteNode, bfsCall};
+export {
+  CreateDFS,
+  CreateBFS,
+  create2DArray,
+  createAdjacencyGraph,
+  DeleteNode,
+  bfsCall,
+};
