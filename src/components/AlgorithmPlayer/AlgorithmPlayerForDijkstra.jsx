7e51c33415f2615afcf49c61355760a4076286b3
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-07-06 22:12:00 +0530

    feat: Animated Dijksta's frame by frame with play back, forward and bachwards button along with shortest path highlight Co-authored-by: JKrishnaKaarthik <JKrishnaKaarthik@users.noreply.github.com>

--- src/components/AlgorithmPlayer/AlgorithmPlayerForDijkstra.jsx
+++ src/components/AlgorithmPlayer/AlgorithmPlayerForDijkstra.jsx
@@ -6,17 +6,20 @@ export default function AlgorithmPlayerForDijkstra({
   currentAlgorithm,
   steps,
   setMyNode,
+  setSelections,
 }) {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [intervalId, setIntervalId] = useState(null);
 
-  function colorNodes(visitedNodes, currentNodeId) {
+  function colorNodes(visitedNodes, currentNodeId, path) {
     setMyNode((currentNodes) => {
       const newNodes = currentNodes.map((node) => {
         let color;
         const nodeIdAsNumber = parseInt(node.id, 10); // Convert node.id to a number
-        if (nodeIdAsNumber === currentNodeId) {
+        if (path.includes(nodeIdAsNumber)) {
+          color = "green"; // Color for the nodes in the path
+        } else if (nodeIdAsNumber === currentNodeId) {
           color = "#1C7C54"; // Color for the current node
         } else if (visitedNodes.includes(nodeIdAsNumber)) {
           color = "red"; // Color for visited nodes
@@ -32,11 +35,28 @@ export default function AlgorithmPlayerForDijkstra({
     });
   }
 
+  function highlightPath(path) {
+    console.log(path);
+    let prev = path[0];
+    let edgeIds = [];
+    for (let i = 1; i < path.length; i++) {
+      let val1 = prev + "->" + path[i];
+      edgeIds.push(val1);
+      const val2 = path[i] + "->" + prev;
+      edgeIds.push(val2);
+      prev = path[i];
+      console.log(edgeIds);
+    }
+    setSelections(edgeIds);
+  }
+
   const goForward = () => {
     setCurrentStepIndex((prevIndex) => {
       const newIndex = Math.min(prevIndex + 1, steps.length - 1);
       // Check if we've reached the last step after updating
       if (newIndex === steps.length - 1) {
+        // Highlight the path when the last step is reached
+        highlightPath(steps[newIndex].path);
         setIsPlaying(false);
       }
       return newIndex;
@@ -56,7 +76,7 @@ export default function AlgorithmPlayerForDijkstra({
       // Set up an interval that calls goForward every 1 second
       const id = setInterval(() => {
         goForward();
-      }, 1000);
+      }, 1500);
       setIntervalId(id); // Store the interval ID
     } else {
       // Clear the interval if isPlaying is toggled to false
@@ -75,11 +95,15 @@ export default function AlgorithmPlayerForDijkstra({
   }, [isPlaying]);
 
   useEffect(() => {
-    if (currentAlgorithm === "dijkstras") {
+    if (currentAlgorithm === "dijkstra's") {
       if (steps.length > 0) {
         const currentStep = steps[currentStepIndex];
         // Assuming colorNodes function is defined and available in this scope
-        colorNodes(currentStep.visitedNodes, currentStep.currentNode);
+        colorNodes(
+          currentStep.visitedNodes,
+          currentStep.currentNode,
+          currentStep.path
+        );
       }
 
       if (steps.length === 0) {
