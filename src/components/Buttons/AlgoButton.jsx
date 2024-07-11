5540c425397f53600916e622a1d2e72dae16ddb1
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-07-11 22:15:23 +0530

    refactor: Update AlgoButton styling and add disabled state
    + Update the styling of the AlgoButton component to use a different background color and border. Also, add a rounded-3xl class to give the button a rounded shape. Additionally, include a disabled state with appropriate styling for disabled buttons.

--- src/components/Buttons/AlgoButton.jsx
+++ src/components/Buttons/AlgoButton.jsx
@@ -9,7 +9,7 @@ export default function AlgoButton({ onClick, disabled, children }) {
         disabled={disabled}
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
-        className="bg-[#878C8F] m-4 min-w-fit min-h-fit h-10 hover:bg-[#A4969B] text-white font-bold py-2 px-4 rounded disabled:bg-gray-400 disabled:cursor-not-allowed disabled:hover:bg-gray-400 disabled:text-gray-700"
+        className="bg-[#3A506B] border-[1px] border-white m-4 min-w-fit min-h-fit h-10 hover:bg-[#225858] text-white font-bold py-2 px-4 rounded-3xl disabled:bg-gray-400 disabled:cursor-not-allowed disabled:hover:bg-gray-400 disabled:text-gray-700"
       >
         {children}
       </motion.button>
