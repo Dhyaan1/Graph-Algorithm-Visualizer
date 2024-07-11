3e047fa8c49b19ae8484ea7af90c45f18d8445de
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-07-11 22:14:55 +0530

    refactor: Update styling for destination and source node display

--- src/components/SelectedNodes/DestinationNodeDisplay.jsx
+++ src/components/SelectedNodes/DestinationNodeDisplay.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
+
 export default function DestinationNodeDisplay({ destinationNode }) {
   return (
     <>
-      <div className="bg-gray-200 px-4 rounded-2xl">
-        <h2 className="text-xl font-bold">Destination Node</h2>
+      <div className="bg-[#0B132B] border-[2px] border-[#3A506B] px-4 rounded-2xl">
+        <h2 className="text-white text-xl font-bold">Destination Node</h2>
         {destinationNode === null ? (
-          <p className="text-gray-600">No Destination Node Selected</p>
+          <p className="text-white">No Destination Node Selected</p>
         ) : (
-          <p className="text-gray-600">Node ID: {destinationNode}</p>
+          <p className="text-white">Node ID: {destinationNode}</p>
         )}
       </div>
     </>
--- src/components/SelectedNodes/SourceNodeDisplay.jsx
+++ src/components/SelectedNodes/SourceNodeDisplay.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
+
 export default function SourceNodeDisplay({ sourceNode }) {
   return (
     <>
-      <div className="bg-gradient-to-r from-[#4dc9e6] to-[#210cae] px-4 rounded-2xl">
-        <h2 className="text-xl font-bold">Source Node</h2>
+      <div className="bg-[#0B132B] border-[2px] border-[#3A506B] px-4 rounded-2xl">
+        <h2 className="text-white text-xl font-bold">Source Node</h2>
         {sourceNode === null ? (
-          <p className="text-gray-600">No Source Node Selected</p>
+          <p className="text-white">No Source Node Selected</p>
         ) : (
-          <p className="text-gray-600">Node ID: {sourceNode}</p>
+          <p className="text-white">Node ID: {sourceNode}</p>
         )}
       </div>
     </>
