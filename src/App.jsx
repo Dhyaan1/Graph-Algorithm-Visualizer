f6d62ae54f11fc667d21afdc885bc0e01214fd15
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-07-18 16:05:48 +0530

    feat: Add website logo and link to documentation in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import GraphDisplayPanel from "./components/GraphDisplayPanel/GraphDisplayPanel"
 import FunctionalDisplayPanel from "./components/FunctionalDisplayPanel/FunctionalDisplayPanel";
 import OutputDisplayPanel from "./components/OutputDisplayPanel/OutputDisplayPanel";
 import { OutPutContextProvider } from "./components/context/OutPutContextProvider";
+import WebsiteLogo from "./assets/icon-1.svg";
 
 function App() {
   const graphRef = useRef(null);
@@ -38,6 +39,32 @@ function App() {
 
   return (
     <>
+      <a
+        href="https://drive.google.com/file/d/13_UUkqcGD7KFx85PR4pgCD9R1sAPelnN/view?usp=drive_link"
+        target="_blank"
+        className="m-4 absolute top-20 max-[711px]:top-0 max-[551px]:relative right-0 z-10 inline-flex items-center justify-center gap-4 px-5 py-2 text-base font-medium rounded-lg text-gray-400 bg-[#0B132B] border-[2px] border-[#3A506B] hover:bg-gray-700 hover:text-white"
+      >
+        <img className="w-8 h-8" src={WebsiteLogo} alt="InspectElementLogo1" />
+        <div className="flex items-center justify-center">
+          <span className="w-full">Read the Docs</span>
+          <svg
+            className="w-4 h-4 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </div>
+      </a>
+
       <OutPutContextProvider>
         <div className="h-screen w-screen relative flex flex-col max-[950px]:h-auto">
           <FunctionalDisplayPanel
